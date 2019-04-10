d677857e45529da31cb8285d68df8516974ec48c
wuriyanto <wuriyanto48@yahoo.co.id>
2019-04-10 13:16:46 +0700

    automatically get extension

--- main.go
+++ main.go
@@ -8,19 +8,26 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"time"
 )
 
 func main() {
 	var (
 		outputName string
+		fileName   string
 	)
 
-	flag.StringVar(&outputName, "output", "", "output name")
-	flag.StringVar(&outputName, "o", "", "output name")
+	flag.StringVar(&outputName, "output", "output", "output name")
+	flag.StringVar(&outputName, "o", "output", "output name")
 
 	flag.Usage = func() {
-		fmt.Println("yowes [url] -o my-file")
+		fmt.Println()
+		fmt.Println("Usage: ")
+		fmt.Println("yowes [url]")
+		fmt.Println("yowes -o my-file [url]")
+		fmt.Println("---------------------------")
+		fmt.Println()
 	}
 
 	flag.Parse()
@@ -32,11 +39,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(args)
-
 	url := args[0]
 
-	fmt.Println(outputName)
+	if !isValidURL(url) {
+		fmt.Println("url empty or invalid")
+		os.Exit(1)
+	}
 
 	// get response from given url
 	response, err := get(url)
@@ -47,8 +55,15 @@ func main() {
 
 	defer response.Body.Close()
 
+	ext := getExtension(url)
+	if len(ext) > 0 {
+		fileName = fmt.Sprintf("%s.%s", outputName, ext)
+	} else {
+		fileName = outputName
+	}
+
 	// create output file
-	file, err := os.Create(outputName)
+	file, err := os.Create(fileName)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -89,8 +104,20 @@ func read(in io.Reader, out io.Writer) error {
 	return nil
 }
 
-func isValidURL(url string) bool {
+func getExtension(urlParam string) string {
+	urls := strings.Split(urlParam, "/")
+	lastURLIdx := urls[len(urls)-1]
+
+	exts := strings.Split(lastURLIdx, ".")
+	if len(exts) < 2 {
+		return ""
+	}
+
+	return exts[1]
+}
+
+func isValidURL(urlParam string) bool {
 	urlRegex := "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$"
 	re := regexp.MustCompile(urlRegex)
-	return re.MatchString(url)
+	return re.MatchString(urlParam)
 }
